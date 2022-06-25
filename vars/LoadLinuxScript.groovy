335da44ac1d42cb10f110ca40223072437ab21a5
vcloud <vcloud@kubemaster.vm>
2022-06-25 14:52:02 +0300

    updated

--- vars/LoadLinuxScript.groovy
+++ vars/LoadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "${config.name}"    
+  def scriptcontents = libraryResource "com/scripts/${config.name}"    
   writeFile file: "${config.name}", text: scriptcontents 
   sh "chmod a+x ./${config.name}"
 } 
